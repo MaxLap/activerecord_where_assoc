b0980c91aee20d966fc0072db7066ae49f6b6521
Maxime Lapointe <hunter_spawn@hotmail.com>
2017-08-03 10:57:04 -0400

    Moved TestHelpers module to load_test_env Makes it available in console.

--- test/support/load_test_env.rb
+++ test/support/load_test_env.rb
@@ -10,3 +10,18 @@ require "active_support"
 require_relative "database_setup"
 require_relative "schema"
 require_relative "models"
+
+
+module TestHelpers
+  def self.condition_value_result_for(*source_associations)
+    source_associations.map do |source_association|
+      model_name, association = source_association.to_s.split("_", 2)
+      value = BaseTestRecord.model_associations_conditions[[model_name, association]]
+
+      raise "No condition #{source_association} found" if value.nil?
+
+      value
+    end.inject(:*)
+  end
+  delegate :condition_value_result_for, to: "TestHelpers"
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,21 +18,6 @@ require "minitest/autorun"
 require_relative "support/custom_asserts"
 
 
-module TestHelpers
-  def self.condition_value_result_for(*source_associations)
-    source_associations.map do |source_association|
-      model_name, association = source_association.to_s.split("_", 2)
-      value = BaseTestRecord.model_associations_conditions[[model_name, association]]
-
-      raise "No condition #{source_association} found" if value.nil?
-
-      value
-    end.inject(:*)
-  end
-  delegate :condition_value_result_for, to: "TestHelpers"
-end
-
-
 class MyMinitestSpec < Minitest::Spec
   include TestHelpers
 
