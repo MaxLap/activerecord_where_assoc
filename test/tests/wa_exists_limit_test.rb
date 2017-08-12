877e45d8394a0540173c8fc9616deb84ff5103c3
Maxime Lapointe <hunter_spawn@hotmail.com>
2017-08-12 01:29:54 -0400

    Forgotten #limit(0) test

--- test/tests/wa_exists_limit_test.rb
+++ test/tests/wa_exists_limit_test.rb
@@ -5,22 +5,28 @@ require "test_helper"
 describe "wa_exists" do
   let(:s0) { S0.create_default! }
 
-  it "has_many with #limit(0), never matches" do
+  it "has_many with #limit(0) never matches" do
     s0.create_assoc!(:m1, :S0_m1)
 
     assert_exists_without_matching(:m1) { |s| s.limit(0) }
   end
 
-  it "has_one with #limit(0), never matches" do
+  it "has_one with #limit(0) never matches" do
     skip if Test::SelectedDBHelper == Test::MySQL
     s0.create_assoc!(:o1, :S0_o1)
 
     assert_exists_without_matching(:o1) { |s| s.limit(0) }
   end
 
-  it "belongs_to with #limit(0), never matches" do
+  it "belongs_to with #limit(0) never matches" do
     s0.create_assoc!(:b1, :S0_b1)
 
     assert_exists_without_matching(:b1) { |s| s.limit(0) }
+  end
+
+  it "has_and_belongs_to_many with #limit(0) never matches" do
+    s0.create_assoc!(:z1, :S0_z1)
+
+    assert_exists_without_matching(:z1) { |s| s.limit(0) }
   end
 end
