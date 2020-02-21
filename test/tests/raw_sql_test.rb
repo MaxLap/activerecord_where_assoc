76305982cf6fa62d3dbffdb47bd599a62ebfa7b5
Maxime Lapointe <hunter_spawn@hotmail.com>
2020-02-21 14:55:19 -0500

    Fix raw_sql_test for cross database

--- test/tests/raw_sql_test.rb
+++ test/tests/raw_sql_test.rb
@@ -6,29 +6,29 @@ require_relative "../test_helper"
 describe "SQL methods" do
   it "#assoc_exists_sql generates expected sql" do
     sql = S0.assoc_exists_sql(:m1)
-    expected_sql = %(EXISTS (SELECT 1 FROM "s1s" WHERE (s1s.s1s_column % __NUMBER__ = 0) AND (s1s.s1s_column % __NUMBER__ = 0) AND "s1s"."s0_id" = "s0s"."id"))
-    expected_sql_regex = Regexp.new(Regexp.quote(expected_sql).gsub("__NUMBER__", '\d+'))
+    expected_sql = %(EXISTS (SELECT 1 FROM "s1s" WHERE ("s1s"."s1s_column" % __NUMBER__ = 0) AND ("s1s"."s1s_column" % __NUMBER__ = 0) AND "s1s"."s0_id" = "s0s"."id"))
+    expected_sql_regex = Regexp.new(Regexp.quote(expected_sql).gsub("__NUMBER__", '\d+').gsub('"', '["`]?'))
     assert_match expected_sql_regex, sql
   end
 
   it "#assoc_not_exists_sql generates expected sql" do
     sql = S0.assoc_not_exists_sql(:m1)
-    expected_sql = %(NOT EXISTS (SELECT 1 FROM "s1s" WHERE (s1s.s1s_column % __NUMBER__ = 0) AND (s1s.s1s_column % __NUMBER__ = 0) AND "s1s"."s0_id" = "s0s"."id"))
-    expected_sql_regex = Regexp.new(Regexp.quote(expected_sql).gsub("__NUMBER__", '\d+'))
+    expected_sql = %(NOT EXISTS (SELECT 1 FROM "s1s" WHERE ("s1s"."s1s_column" % __NUMBER__ = 0) AND ("s1s"."s1s_column" % __NUMBER__ = 0) AND "s1s"."s0_id" = "s0s"."id"))
+    expected_sql_regex = Regexp.new(Regexp.quote(expected_sql).gsub("__NUMBER__", '\d+').gsub('"', '["`]?'))
     assert_match expected_sql_regex, sql
   end
 
   it "#only_assoc_count_sql generates expected sql" do
     sql = S0.only_assoc_count_sql(:m1)
-    expected_sql = %(COALESCE((SELECT COUNT(*) FROM "s1s" WHERE (s1s.s1s_column % __NUMBER__ = 0) AND (s1s.s1s_column % __NUMBER__ = 0) AND "s1s"."s0_id" = "s0s"."id"), 0))
-    expected_sql_regex = Regexp.new(Regexp.quote(expected_sql).gsub("__NUMBER__", '\d+'))
+    expected_sql = %(COALESCE((SELECT COUNT(*) FROM "s1s" WHERE ("s1s"."s1s_column" % __NUMBER__ = 0) AND ("s1s"."s1s_column" % __NUMBER__ = 0) AND "s1s"."s0_id" = "s0s"."id"), 0))
+    expected_sql_regex = Regexp.new(Regexp.quote(expected_sql).gsub("__NUMBER__", '\d+').gsub('"', '["`]?'))
     assert_match expected_sql_regex, sql
   end
 
   it "#compare_assoc_count_sql generates expected sql" do
     sql = S0.compare_assoc_count_sql(5, :<, :m1)
-    expected_sql = %((5) < COALESCE((SELECT COUNT(*) FROM "s1s" WHERE (s1s.s1s_column % __NUMBER__ = 0) AND (s1s.s1s_column % __NUMBER__ = 0) AND "s1s"."s0_id" = "s0s"."id"), 0))
-    expected_sql_regex = Regexp.new(Regexp.quote(expected_sql).gsub("__NUMBER__", '\d+'))
+    expected_sql = %((5) < COALESCE((SELECT COUNT(*) FROM "s1s" WHERE ("s1s"."s1s_column" % __NUMBER__ = 0) AND ("s1s"."s1s_column" % __NUMBER__ = 0) AND "s1s"."s0_id" = "s0s"."id"), 0))
+    expected_sql_regex = Regexp.new(Regexp.quote(expected_sql).gsub("__NUMBER__", '\d+').gsub('"', '["`]?'))
     assert_match expected_sql_regex, sql
   end
 end
